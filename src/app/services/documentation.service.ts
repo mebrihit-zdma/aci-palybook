a15d55840c63134ee8f3ffc04e0654a166bdb897
mebrihit-z <mebrihit@cxstudios.io>
2025-05-22 16:16:20 -0400

    creating DocumentationService

---
+++ src/app/services/documentation.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentationService {
+  skipTooltipValue: boolean = false;
+
+  documentationLandingPage = false;
+  documentationGeneratingPage = false; 
+  documentationGeneratedPage = false; 
+
+  setDocumentationLandingPage(documentationLandingPage: boolean) {
+    this.documentationLandingPage = documentationLandingPage ;
+  }
+
+  getDocumentationLandingPage():  boolean{
+    return this.documentationLandingPage;
+  }
+
+  setDocumentationGeneratingPage(documentationGeneratingPage: boolean) {
+    this.documentationGeneratingPage = documentationGeneratingPage ;
+  }
+
+  getDocumentationGeneratingPage():  boolean{
+    return this.documentationGeneratingPage;
+  }
+
+  setDocumentationGeneratedPage(documentationGeneratedPage: boolean) {
+    this.documentationGeneratedPage = documentationGeneratedPage ;
+  }
+
+  getDocumentationGeneratedPage():  boolean{
+    return this.documentationGeneratedPage;
+  }
+}
