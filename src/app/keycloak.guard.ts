6d6352a7fffb410f97fa487ea28bc11b1ffe5add
mebrihit-z <mebrihit@cxstudios.io>
2025-05-29 17:59:06 -0400

    cleaning css

--- src/app/keycloak.guard.ts
+++ src/app/keycloak.guard.ts
@@ -1,6 +1,6 @@
-// src/app/keycloak.guard.ts
+
 import { createAuthGuard } from 'keycloak-angular';
 
 export const authGuard = createAuthGuard(async () => {
-  return true; // or put your custom logic here
+  return true;
 });
