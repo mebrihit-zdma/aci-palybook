9eba1137b61143a206f959c2501a8a66f09aee29
mebrihit-z <mebrihit@cxstudios.io>
2025-05-30 12:57:45 -0400

    cleaning css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,12 @@
 
 import { RouterOutlet } from '@angular/router';
-import { Component, OnInit, Inject, OnDestroy,} from '@angular/core';
-import { Subject } from 'rxjs';
-import { filter, takeUntil } from 'rxjs/operators';
-import { RouterModule } from '@angular/router';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserService } from './services/user.service';
-import { LoginService } from './services/login.service';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterModule, CommonModule],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
