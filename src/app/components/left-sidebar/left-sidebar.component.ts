295cd027e32689196a8f07b51d27efe5e07b34fb
mebrihit-z <mebrihit@cxstudios.io>
2025-08-07 16:26:38 -0400

    Moving google fonts and  icons to index.html

--- src/app/components/left-sidebar/left-sidebar.component.ts
+++ src/app/components/left-sidebar/left-sidebar.component.ts
@@ -21,12 +21,12 @@ export class LeftSidebarComponent {
   
   loginDisplay: boolean = false;
 
-  // constructor(private userService: UserService, private loginService: LoginService, private searchChatService: SearchChatService, private chatService: ChatService, private documentationService: DocumentationService, private router: Router, private authService: MsalService) {
-
-  // }
-  constructor(private userService: UserService, private loginService: LoginService, private searchChatService: SearchChatService, private chatService: ChatService, private documentationService: DocumentationService, private router: Router) {
+  constructor(private userService: UserService, private loginService: LoginService, private searchChatService: SearchChatService, private chatService: ChatService, private documentationService: DocumentationService, private router: Router, private authService: MsalService) {
 
   }
+  // constructor(private userService: UserService, private loginService: LoginService, private searchChatService: SearchChatService, private chatService: ChatService, private documentationService: DocumentationService, private router: Router) {
+
+  // }
   
   userName: string | null = 'User Name';
   userRole: string | null = 'Product Owner';
@@ -128,9 +128,9 @@ export class LeftSidebarComponent {
   }
 
   // Log the user out
-  // logout() {
-  //   if(this.loginDisplay){
-  //     this.authService.logoutRedirect();
-  //   }
-  // }
+  logout() {
+    if(this.loginDisplay){
+      this.authService.logoutRedirect();
+    }
+  }
 }
